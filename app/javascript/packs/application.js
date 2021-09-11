d120f7af6f4697989c4c69dbab807f9caaf54fb9
ringo-panda <haru@Harus-Macbook.local>
2021-09-11 17:06:37 +0900

    [Add]:BootstrapとjQueryを追加
    webpackerにて導入

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -8,6 +8,10 @@ import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
 import "channels"
 
+//bootstrapとjQueryの設定
+import 'bootstrap';
+import '../stylesheets/application';
+
 Rails.start()
 Turbolinks.start()
 ActiveStorage.start()
--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,3 +1,16 @@
 const { environment } = require('@rails/webpacker')
 
+
+//BootstrapとjQueryの設定
+const webpack = require('webpack')
+environment.plugins.prepend(
+  'Provide',
+  new webpack.ProvidePlugin({
+    $: 'jquery',
+    jQuery: 'jquery',
+    Popper: 'popper.js'
+  })
+)
+
+
 module.exports = environment
